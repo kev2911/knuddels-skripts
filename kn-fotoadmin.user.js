4659c1e06ade7fac9f3fab3747ee8f81224fcd71
kev2911 <k.haeringer@gmail.com>
2026-07-03 21:53:06 +0200

    Rename script and update version number

--- kn-fotoadmin.user.js
+++ kn-fotoadmin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Extended Foto
+// @name         kn-fotoadmin
 // @namespace    https://photo.knuddels.de/
-// @version      1.05
+// @version      1.04
 // @description  Fotoadministration-Helfer für Knuddels.de (KI-Check, neues Layout, Nick kopieren, Melden im Hintergrund)
 // @author       Kev
 // @match        https://photo.knuddels.de/photos-admin*
